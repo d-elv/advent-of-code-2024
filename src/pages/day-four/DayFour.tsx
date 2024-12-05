65cfa67131bf8626e6ea60c6553c3d10895aeded
Daniel Elvey <delvey21@hotmail.co.uk>
2024-12-05 15:14:26 +0000

    Added grid search code for part 2. Now to adapt it to the actual question in part 2 of day 4

--- src/pages/day-four/DayFour.tsx
+++ src/pages/day-four/DayFour.tsx
@@ -7,16 +7,16 @@ import ReturnHome from "../../components/ReturnToHome";
 // XMAS.S
 // .X....`;
 
-// const input = `MMMSXXMASM
-// MSAMXMSMSA
-// AMXSXMAAMM
-// MSAMASMSMX
-// XMASAMXAMM
-// XXAMMXXAMA
-// SMSMSASXSS
-// SAXAMASAAA
-// MAMMMXMMMM
-// MXMXAXMASX`;
+const input = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`;
 
 function getPartOneAnswer(input: string) {
   // Turns input string into 2D array
@@ -24,11 +24,11 @@ function getPartOneAnswer(input: string) {
     return row.split("");
   });
 
-  let totalXmases = 0;
+  let xmasCount = 0;
   const columns = matrix[0].length;
 
   function checkForwards(row: string[]) {
-    let totalXmases = 0;
+    let xmasCount = 0;
     let xmas = "";
     for (let index = 0; index < row.length; index++) {
       switch (row[index]) {
@@ -55,7 +55,7 @@ function getPartOneAnswer(input: string) {
           break;
         case "S":
           if (xmas === "XMA") {
-            totalXmases += 1;
+            xmasCount += 1;
             xmas = "";
           } else {
             xmas = "";
@@ -65,25 +65,25 @@ function getPartOneAnswer(input: string) {
           xmas = "";
       }
     }
-    return totalXmases;
+    return xmasCount;
   }
 
   function checkReverse(row: string[]) {
-    let totalXmases = 0;
+    let xmasCount = 0;
     const reversedRow = row.toReversed();
-    totalXmases += checkForwards(reversedRow);
-    return totalXmases;
+    xmasCount += checkForwards(reversedRow);
+    return xmasCount;
   }
 
   function getAndCheckVerticals(matrix: string[][]) {
-    let totalXmases = 0;
+    let xmasCount = 0;
     const getColumns = (matrix: string[][], index: number) =>
       matrix.map((x) => x[index]);
     for (let index = 0; index < columns; index++) {
-      totalXmases += checkForwards(getColumns(matrix, index));
-      totalXmases += checkReverse(getColumns(matrix, index));
+      xmasCount += checkForwards(getColumns(matrix, index));
+      xmasCount += checkReverse(getColumns(matrix, index));
     }
-    return totalXmases;
+    return xmasCount;
   }
 
   function getDiagonals(matrix: string[][]) {
@@ -174,46 +174,240 @@ function getPartOneAnswer(input: string) {
 
   for (let row of matrix) {
     // finds the horizontal XMAS occurrences
-    totalXmases += checkForwards(row);
-    totalXmases += checkReverse(row);
+    xmasCount += checkForwards(row);
+    xmasCount += checkReverse(row);
   }
 
-  totalXmases += getAndCheckVerticals(matrix);
+  xmasCount += getAndCheckVerticals(matrix);
   // Someone should really write the logic of how the vertical arrays are processed to match the horizontals and diagonals
   const { leftToRightDiagonalArrays, rightToLeftDiagonalArrays } =
     getDiagonals(matrix);
 
   for (let row of leftToRightDiagonalArrays) {
-    totalXmases += checkForwards(row);
-    totalXmases += checkReverse(row);
+    xmasCount += checkForwards(row);
+    xmasCount += checkReverse(row);
   }
   for (let row of rightToLeftDiagonalArrays) {
-    totalXmases += checkForwards(row);
-    totalXmases += checkReverse(row);
+    xmasCount += checkForwards(row);
+    xmasCount += checkReverse(row);
   }
 
-  return totalXmases;
+  return xmasCount;
 }
 
-function getPartTwoAnswer(input: string) {}
+function getPartTwoAnswer(input: string) {
+  // Turns input string into 2D array
+  const matrix = input.split("\n").map((row) => {
+    return row.split("");
+  });
+  console.log(matrix);
+
+  let xmasCount = 0;
+
+  function checkNorth(rowIndex: number, colIndex: number) {
+    // console.log(matrix[rowIndex][colIndex], rowIndex, colIndex);
+
+    if (matrix[rowIndex - 3] === undefined) return false;
+
+    if (matrix[rowIndex - 0][colIndex] === "X") {
+      if (
+        matrix[rowIndex - 1][colIndex] === "M" &&
+        matrix[rowIndex - 2][colIndex] === "A" &&
+        matrix[rowIndex - 3][colIndex] === "S"
+      ) {
+        console.log("North");
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function checkNorthEast(rowIndex: number, colIndex: number) {
+    // console.log(matrix[rowIndex][colIndex], rowIndex, colIndex);
+    if (
+      matrix[rowIndex - 3] === undefined ||
+      matrix[rowIndex - 3][colIndex + 3] === undefined
+    )
+      return false;
+
+    if (matrix[rowIndex - 0][colIndex + 0] === "X") {
+      if (
+        matrix[rowIndex - 1][colIndex + 1] === "M" &&
+        matrix[rowIndex - 2][colIndex + 2] === "A" &&
+        matrix[rowIndex - 3][colIndex + 3] === "S"
+      ) {
+        console.log("North East");
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function checkEast(rowIndex: number, colIndex: number) {
+    // console.log(matrix[rowIndex][colIndex], rowIndex, colIndex);
+
+    if (matrix[colIndex + 3] === undefined) return false;
+
+    if (matrix[rowIndex][colIndex + 0] === "X") {
+      if (
+        matrix[rowIndex][colIndex + 1] === "M" &&
+        matrix[rowIndex][colIndex + 2] === "A" &&
+        matrix[rowIndex][colIndex + 3] === "S"
+      ) {
+        console.log("East");
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function checkSouthEast(rowIndex: number, colIndex: number) {
+    // console.log(matrix[rowIndex][colIndex], rowIndex, colIndex);
+    if (
+      matrix[rowIndex + 3] === undefined ||
+      matrix[rowIndex + 3][colIndex + 3] === undefined
+    )
+      return false;
+
+    if (matrix[rowIndex + 0][colIndex + 0] === "X") {
+      if (
+        matrix[rowIndex + 1][colIndex + 1] === "M" &&
+        matrix[rowIndex + 2][colIndex + 2] === "A" &&
+        matrix[rowIndex + 3][colIndex + 3] === "S"
+      ) {
+        console.log("South East");
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function checkSouth(rowIndex: number, colIndex: number) {
+    // console.log(matrix[rowIndex][colIndex], rowIndex, colIndex);
+
+    if (matrix[rowIndex + 3] === undefined) return false;
+
+    if (matrix[rowIndex + 0][colIndex] === "X") {
+      if (
+        matrix[rowIndex + 1][colIndex] === "M" &&
+        matrix[rowIndex + 2][colIndex] === "A" &&
+        matrix[rowIndex + 3][colIndex] === "S"
+      ) {
+        console.log("South");
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function checkSouthWest(rowIndex: number, colIndex: number) {
+    // console.log(matrix[rowIndex][colIndex], rowIndex, colIndex);
+    if (
+      matrix[rowIndex + 3] === undefined ||
+      matrix[rowIndex + 3][colIndex - 3] === undefined
+    )
+      return false;
+
+    if (matrix[rowIndex + 0][colIndex - 0] === "X") {
+      if (
+        matrix[rowIndex + 1][colIndex - 1] === "M" &&
+        matrix[rowIndex + 2][colIndex - 2] === "A" &&
+        matrix[rowIndex + 3][colIndex - 3] === "S"
+      ) {
+        console.log("South West");
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function checkWest(rowIndex: number, colIndex: number) {
+    // console.log(matrix[rowIndex][colIndex], rowIndex, colIndex);
+
+    if (matrix[colIndex - 3] === undefined) return false;
+
+    if (matrix[rowIndex][colIndex - 0] === "X") {
+      if (
+        matrix[rowIndex][colIndex - 1] === "M" &&
+        matrix[rowIndex][colIndex - 2] === "A" &&
+        matrix[rowIndex][colIndex - 3] === "S"
+      ) {
+        console.log("West");
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function checkNorthWest(rowIndex: number, colIndex: number) {
+    // console.log(matrix[rowIndex][colIndex], rowIndex, colIndex);
+    if (
+      matrix[rowIndex - 3] === undefined ||
+      matrix[rowIndex - 3][colIndex - 3] === undefined
+    )
+      return false;
+
+    if (matrix[rowIndex - 0][colIndex - 0] === "X") {
+      if (
+        matrix[rowIndex - 1][colIndex - 1] === "M" &&
+        matrix[rowIndex - 2][colIndex - 2] === "A" &&
+        matrix[rowIndex - 3][colIndex - 3] === "S"
+      ) {
+        console.log("North West");
+        return true;
+      }
+    }
+    return false;
+  }
+
+  matrix.forEach((line, rowIndex) => {
+    line.forEach((cell, columnIndex) => {
+      if (checkNorth(rowIndex, columnIndex)) {
+        xmasCount += 1;
+      }
+      if (checkNorthEast(rowIndex, columnIndex)) {
+        xmasCount += 1;
+      }
+      if (checkEast(rowIndex, columnIndex)) {
+        xmasCount += 1;
+      }
+      if (checkSouthEast(rowIndex, columnIndex)) {
+        xmasCount += 1;
+      }
+      if (checkSouth(rowIndex, columnIndex)) {
+        xmasCount += 1;
+      }
+      if (checkSouthWest(rowIndex, columnIndex)) {
+        xmasCount += 1;
+      }
+      if (checkWest(rowIndex, columnIndex)) {
+        xmasCount += 1;
+      }
+      if (checkNorthWest(rowIndex, columnIndex)) {
+        xmasCount += 1;
+      }
+    });
+  });
+  return xmasCount;
+}
 
 export default function DayFour() {
-  const [input, setInput] = useState("");
-  const partOneAnswer = getPartOneAnswer(input);
+  // const [input, setInput] = useState("");
+  // const partOneAnswer = getPartOneAnswer(input);
   const partTwoAnswer = getPartTwoAnswer(input);
 
-  useEffect(() => {
-    fetch("/day-four-puzzle-input.txt")
-      .then((response) => response.text())
-      .then((data) => setInput(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/day-four-puzzle-input.txt")
+  //     .then((response) => response.text())
+  //     .then((data) => setInput(data));
+  // }, []);
 
   return (
     <>
       <h1>Day Three</h1>
       <ReturnHome />
-      <h2>The result for Part One is: {partOneAnswer}</h2>
-      {/* <h2>The result for Part Two is: {partTwoAnswer}</h2> */}
+      {/* <h2>The result for Part One is: {partOneAnswer}</h2> */}
+      <h2>The result for Part Two is: {partTwoAnswer}</h2>
     </>
   );
 }
